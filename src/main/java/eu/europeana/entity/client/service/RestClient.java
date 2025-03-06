5cd902f3cc1f14224757e1454f12120c485befb6
Jorge Ortiz <jorge.ortiz@europeana.eu>
2025-03-06 13:15:04 +0100

    MET-6258 obtain entity when an entity redirect happens

--- src/main/java/eu/europeana/entity/client/service/RestClient.java
+++ src/main/java/eu/europeana/entity/client/service/RestClient.java
@@ -1,128 +1,158 @@
 package eu.europeana.entity.client.service;
 
+import static eu.europeana.entity.client.utils.EntityApiConstants.WSKEY;
+
 import eu.europeana.entity.client.exception.MultipleEntityFoundException;
 import eu.europeana.entity.client.exception.TechnicalRuntimeException;
 import eu.europeana.entity.client.model.EntityRetrievalResponse;
 import eu.europeana.entity.client.utils.EntityApiConstants;
 import eu.europeana.entitymanagement.definitions.model.Entity;
+import java.net.URI;
+import java.util.function.Function;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.Exceptions;
 import reactor.core.publisher.Mono;
 
-import java.net.URI;
-import java.util.function.Function;
-
 public class RestClient {
 
-    private static final Logger LOGGER = LogManager.getLogger(RestClient.class);
+  private static final Logger LOGGER = LogManager.getLogger(RestClient.class);
 
-    /**
-     * Methods returns the desired results.
-     * if getLocationHeader is true , return 'location' header value
-     *  which is the non-redirect url for existing Entity (entity ID)
-     *  if getLocationHeader is false, returns the String json response
-     *
-     * @param webClient web client
-     * @param uriBuilderURIFunction url to be executed
-     * @param getLocationHeader true, if location header value is to be retrieved
-     * @param <T> Desired result Type
-     * @return
-     * @throws TechnicalRuntimeException
-     */
-    public <T> T getEntityId(WebClient webClient, Function<UriBuilder, URI> uriBuilderURIFunction, boolean getLocationHeader) throws TechnicalRuntimeException {
-        try {
-            WebClient.ResponseSpec result = executeGet(webClient, uriBuilderURIFunction);
-            if (getLocationHeader) {
-                return (T) result.toBodilessEntity()
-                        .flatMap(voidResponseEntity ->
-                                Mono.justOrEmpty(voidResponseEntity.getHeaders().getFirst(EntityApiConstants.HEADER_LOCATION)))
-                        .block();
-                 }
-                 return (T) result
-                         .bodyToMono(String.class)
-                         .block();
-        } catch (Exception e) {
-            /*
-             * Spring WebFlux wraps exceptions in ReactiveError (see Exceptions.propagate())
-             * So we need to unwrap the underlying exception, for it to be handled by callers of this method
-             **/
-            Throwable t = Exceptions.unwrap(e);
-            if (t instanceof TechnicalRuntimeException) {
-                throw new TechnicalRuntimeException("User is not authorised to perform this action");
-            }
-            if (t instanceof MultipleEntityFoundException) {
-                LOGGER.debug("Multiple Entity found - {} ", e.getMessage());
-                return null ;
-            }
-            // all other exception should be logged and null response should be returned
-            LOGGER.debug("Entity API Client call failed - {}", e.getMessage());
-            return null ;
-        }
+  /**
+   * Methods returns the desired results. if getLocationHeader is true , return 'location' header value which is the non-redirect
+   * url for existing Entity (entity ID) if getLocationHeader is false, returns the String json response
+   *
+   * @param webClient web client
+   * @param uriBuilderURIFunction url to be executed
+   * @param getLocationHeader true, if location header value is to be retrieved
+   * @param <T> Desired result Type
+   * @return
+   * @throws TechnicalRuntimeException
+   */
+  public <T> T getEntityId(WebClient webClient, Function<UriBuilder, URI> uriBuilderURIFunction, boolean getLocationHeader)
+      throws TechnicalRuntimeException {
+    try {
+
+      if (getLocationHeader) {
+        return (T) executeGet(webClient, uriBuilderURIFunction, ResponseEntity.class)
+            .flatMap(voidResponseEntity ->
+                Mono.justOrEmpty(voidResponseEntity.getHeaders().getFirst(EntityApiConstants.HEADER_LOCATION)))
+            .block();
+      }
+      return (T) executeGet(webClient, uriBuilderURIFunction, String.class).block();
+    } catch (Exception e) {
+      /*
+       * Spring WebFlux wraps exceptions in ReactiveError (see Exceptions.propagate())
+       * So we need to unwrap the underlying exception, for it to be handled by callers of this method
+       **/
+      Throwable t = Exceptions.unwrap(e);
+      if (t instanceof TechnicalRuntimeException) {
+        throw new TechnicalRuntimeException("User is not authorised to perform this action");
+      }
+      if (t instanceof MultipleEntityFoundException) {
+        LOGGER.debug("Multiple Entity found - {} ", e.getMessage());
+        return null;
+      }
+      // all other exception should be logged and null response should be returned
+      LOGGER.debug("Entity API Client call failed - {}", e.getMessage());
+      return null;
     }
+  }
 
-    /**
-     * Returns the Single Entity class or List of Entities
-     * @param webClient
-     * @param uriBuilderURIFunction
-     * @param jsonBody if null, executes Get EM retrieval method.
-     *                 If present executes the multiple Entity Retrieval method.
-     * @return
-     */
-    public <T> T getEntities(WebClient webClient, Function<UriBuilder, URI> uriBuilderURIFunction, String jsonBody) {
-        try {
-            if(jsonBody == null) {
-                WebClient.ResponseSpec result = executeGet(webClient, uriBuilderURIFunction);
-                return (T) result.bodyToMono(Entity.class).block();
-            } else {
-                WebClient.ResponseSpec result = executePost(webClient, uriBuilderURIFunction, jsonBody);
-                return (T) result
-                        .bodyToMono(EntityRetrievalResponse.class)
-                        .block();
-            }
-        } catch (Exception e) {
-            /*
-             * Spring WebFlux wraps exceptions in ReactiveError (see Exceptions.propagate())
-             * So we need to unwrap the underlying exception, for it to be handled by callers of this method
-             **/
-            Throwable t = Exceptions.unwrap(e);
-            if (t instanceof TechnicalRuntimeException) {
-                throw new TechnicalRuntimeException("User is not authorised to perform this action");
-            }
-            // all other exception should be logged and null response should be returned
-            LOGGER.debug("Entity Management Client call failed. {}", e.getMessage());
-            return null ;
-        }
+  /**
+   * Returns the Single Entity class or List of Entities
+   *
+   * @param webClient
+   * @param uriBuilderURIFunction
+   * @param jsonBody if null, executes Get EM retrieval method. If present executes the multiple Entity Retrieval method.
+   * @return
+   */
+  public <T> T getEntities(WebClient webClient, Function<UriBuilder, URI> uriBuilderURIFunction, String jsonBody) {
+    try {
+      if (jsonBody == null) {
+        return (T) executeGet(webClient, uriBuilderURIFunction, Entity.class).block();
+      } else {
+        WebClient.ResponseSpec result = executePost(webClient, uriBuilderURIFunction, jsonBody);
+        return (T) result
+            .bodyToMono(EntityRetrievalResponse.class)
+            .block();
+      }
+    } catch (Exception e) {
+      /*
+       * Spring WebFlux wraps exceptions in ReactiveError (see Exceptions.propagate())
+       * So we need to unwrap the underlying exception, for it to be handled by callers of this method
+       **/
+      Throwable t = Exceptions.unwrap(e);
+      if (t instanceof TechnicalRuntimeException) {
+        throw new TechnicalRuntimeException("User is not authorised to perform this action");
+      }
+      // all other exception should be logged and null response should be returned
+      LOGGER.debug("Entity Management Client call failed. {}", e.getMessage());
+      return null;
     }
+  }
 
-    private WebClient.ResponseSpec executeGet(WebClient webClient, Function<UriBuilder, URI> uriBuilderURIFunction) throws TechnicalRuntimeException {
-       return webClient
-                    .get()
-                    .uri(uriBuilderURIFunction)
-                    .accept(MediaType.APPLICATION_JSON)
-                    .retrieve()
-                    .onStatus(
-                            HttpStatus.UNAUTHORIZED::equals,
-                            response -> response.bodyToMono(String.class).map(TechnicalRuntimeException::new))
-                    .onStatus(HttpStatus.MULTIPLE_CHOICES::equals,
-                            response -> response.bodyToMono(String.class).map(MultipleEntityFoundException::new));
+  private <T> Mono<T> executeGet(WebClient webClient, Function<UriBuilder, URI> uriBuilderURIFunction, Class<T> entityClass)
+      throws TechnicalRuntimeException {
+
+    return webClient
+        .get()
+        .uri(uriBuilderURIFunction)
+        .accept(MediaType.APPLICATION_JSON)
+        .exchangeToMono( response -> {
+          if (response.statusCode() == HttpStatus.UNAUTHORIZED) {
+            return response.bodyToMono(String.class).flatMap( errorBody -> Mono.error(new TechnicalRuntimeException("")));
+          } else if (response.statusCode() == HttpStatus.MULTIPLE_CHOICES) {
+            return response.bodyToMono(String.class).flatMap( errorBody -> Mono.error(new MultipleEntityFoundException("")));
+          } else if (response.statusCode() == HttpStatus.MOVED_PERMANENTLY) {
+            return getResponseFromLocation(webClient, response, entityClass);
+          }  else  {
+            return response.bodyToMono(entityClass);
+          }
+        });
+  }
+
+  private <T> Mono<T> getResponseFromLocation(WebClient webClient, ClientResponse response, Class<T> entityClass) {
+
+    URI location = response.headers().asHttpHeaders().getLocation();
+    String query = response.request().getURI().getQuery();
+    final MultiValueMap<String, String> queryParams = UriComponentsBuilder.newInstance().
+                                                                query(query).build().getQueryParams();
+    String wsKey = queryParams.getFirst(WSKEY);
+    LOGGER.debug("Redirecting to location: {}", location);
+
+    if (location != null && wsKey != null) {
+      return executeGet(webClient,
+          uriBuilder -> uriBuilder.host(response.request().getURI().getAuthority())
+                    .path(location.getPath().replace("/entity",""))
+              .query(location.getQuery())
+                    .build(), entityClass);
+    } else {
+      LOGGER.debug("Redirecting to location empty");
+      return Mono.empty();
     }
+  }
 
-    private WebClient.ResponseSpec executePost(WebClient webClient, Function<UriBuilder, URI> uriBuilderURIFunction, String jsonBody) throws TechnicalRuntimeException {
-        return webClient
-                .post()
-                .uri(uriBuilderURIFunction)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(BodyInserters.fromValue(jsonBody))
-                .retrieve()
-                .onStatus(
-                        HttpStatus.UNAUTHORIZED::equals,
-                        response -> response.bodyToMono(String.class).map(TechnicalRuntimeException::new));
+  private WebClient.ResponseSpec executePost(WebClient webClient, Function<UriBuilder, URI> uriBuilderURIFunction,
+      String jsonBody) throws TechnicalRuntimeException {
+    return webClient
+        .post()
+        .uri(uriBuilderURIFunction)
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(BodyInserters.fromValue(jsonBody))
+        .retrieve()
+        .onStatus(
+            HttpStatus.UNAUTHORIZED::equals,
+            response -> response.bodyToMono(String.class).map(TechnicalRuntimeException::new));
 
-    }
+  }
 }
--- src/test/java/eu/europeana/entity/client/web/EntityClientTest.java
+++ src/test/java/eu/europeana/entity/client/web/EntityClientTest.java
@@ -1,24 +1,49 @@
 package eu.europeana.entity.client.web;
-import static org.junit.jupiter.api.Assertions.*;
-import java.util.List;
-import org.junit.jupiter.api.Test;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
+import eu.europeana.entity.client.config.EntityClientConfiguration;
 import eu.europeana.entitymanagement.definitions.model.Entity;
+import java.util.List;
+import java.util.Properties;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 
 class EntityClientTest {
 
-  @Test
-  void test() {
-    fail("Not yet implemented");
+  private EntityClientApi apiClient;
+
+  @BeforeEach
+  void setUp() {
+    final Properties properties = new Properties();
+    properties.put("entity.management.url", "");
+    properties.put("entity.api.url", "");
+    properties.put("entity.api.key", "");
+    final EntityClientConfiguration clientConfiguration = new EntityClientConfiguration(properties);
+    apiClient = new EntityClientApiImpl(clientConfiguration);
   }
 
   @Test
   void testEnrichment() throws JsonProcessingException {
-    EntityClientApi apiClient = new EntityClientApiImpl();
     List<Entity> enrichments = apiClient.getEnrichment("CulturaItalia", null, "organization", null);
     assertNotNull(enrichments);
     assertFalse(enrichments.isEmpty());
   }
-  
-  
+
+  @Test
+  void testEntityRetrievalWithRedirectAreTheSameEntity() {
+    Entity entityA = apiClient.getEntityById("http://data.europeana.eu/organization/4385");
+    Entity entityB = apiClient.getEntityById("http://data.europeana.eu/organization/1482250000001710507");
+    assertNotNull(entityA);
+    assertNotNull(entityB);
+    assertEquals(entityA.getType(), entityB.getType());
+    assertEquals(entityA.getEntityId(), entityB.getEntityId());
+    assertEquals(entityA.getAbout(), entityB.getAbout());
+    assertEquals(entityA.getContext(), entityB.getContext());
+    assertEquals(entityA.getSameReferenceLinks(), entityB.getSameReferenceLinks());
+  }
+
 }
