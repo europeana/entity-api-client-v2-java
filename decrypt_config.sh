1d8b95040e7a2f50f865ba9c22a259b8c301c5f1
GordeaS <4517853+gsergiu@users.noreply.github.com>
2026-01-20 15:16:35 +0100

    ensure execution permission #EA-4270

--- decrypt_config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Decrypt the file
-#NOTE: execution permissions must be ensured before commiting to github
-mkdir -p /opt/app/config
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PROPS_PASSPHRASE" \
---output /opt/app/config/entity-client.properties entity-client.properties.gpg
